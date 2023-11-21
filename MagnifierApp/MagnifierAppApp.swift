7e40e90e5b163bab3e1c8d347cc071d4db371b80
taekwondodev <taekwondodev@outlook.it>
2023-11-21 16:19:53 +0100

    Initial Commit

---
+++ MagnifierApp/MagnifierAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagnifierAppApp.swift
+//  MagnifierApp
+//
+//  Created by Davide Galdiero on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MagnifierAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
